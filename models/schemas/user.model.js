4e063f9a4d028030dca44a47b9261e5696e10862
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-10-09 15:05:27 +0300

    user model

---
+++ models/schemas/user.model.js
@@ -0,0 +1,52 @@
+module.exports = (db, type) =>
+    db.define(
+        'users',
+        {
+            id: {
+                type: type.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            firstName: {
+                type: type.STRING,
+                allowNull: false,
+            },
+            lastName: {
+                type: type.STRING,
+                allowNull: false,
+            },
+            age: {
+                type: type.INTEGER,
+                allowNull: false,
+            },
+            howFoundUs_id: {
+                type: type.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'howFoundUs',
+                    key: 'id',
+                    onDelete: 'CASCADE',
+                    onUpdate: 'CASCADE',
+                },
+            },
+            verified_boolean: {
+                type: type.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            mobile: {
+                type: type.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: type.STRING,
+                allowNull: false,
+            },
+            createdAt: {
+                type: type.DATE,
+                allowNull: false,
+                defaultValue: type.NOW,
+            },
+        },
+        { timestamps: false },
+    );
