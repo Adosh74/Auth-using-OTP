5431c9adfe8c805631b7d333671c27b39a10d2fc
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-10-09 21:09:32 +0300

    update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const config = require('./config/env.config');
 const routes = require('./routes/index.route');
 const AppError = require('./utils/appError');
 const errorMiddleware = require('./middleware/error.middleware');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const xss = require('xss-clean');
 
 const app = express();
 
@@ -12,9 +15,24 @@ if (config.node_env === 'development') {
     app.use(morgan('dev'));
 }
 
+// middleware for limiting requests
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'To many requests from this IP, please try again in an hour',
+});
+
+app.use('/api', limiter);
+
 // parsing application/json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// middleware for preventing parameter pollution
+app.use(hpp());
+
+// middleware for preventing cross-site scripting attacks (XSS)
+app.use(xss());
 
 // routes
 app.get('/xyz', (req, res) => {
