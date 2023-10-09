d280d18805e0e2f004e81bf910e59bc3eb88a042
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-10-09 19:21:56 +0300

    use global error middleware

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const config = require('./config/env.config');
+const routes = require('./routes/index.route');
+const AppError = require('./utils/appError');
+const errorMiddleware = require('./middleware/error.middleware');
 
 const app = express();
 
@@ -12,5 +15,18 @@ if (config.node_env === 'development') {
 // parsing application/json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.get('/xyz', (req, res) => {
+    res.status(200).send('Server Working!');
+});
+
+app.use('/api', routes);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(errorMiddleware);
 
 module.exports = app;
